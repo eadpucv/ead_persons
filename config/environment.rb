73bf4534a5c54758c33b46ffacf8adf546cf3e08
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-02-25 00:26:28 -0300

    Env dev settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,8 @@ require File.expand_path('../application', __FILE__)
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 
-
-CASClient::Frameworks::Rails::Filter.configure(
-  :cas_base_url => "http://190.208.62.202:443/cas/")
+# CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://190.208.62.202:443/cas/")
+CASClient::Frameworks::Rails::Filter.configure(:cas_base_url => "http://0.0.0.0:4000/cas/")
 
 # Initialize the rails application
 Persons::Application.initialize!
